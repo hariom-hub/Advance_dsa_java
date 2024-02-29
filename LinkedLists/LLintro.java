023975275122f7bba05d152ff3b55dafcd72f6b0
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-02-29 22:04:23 +0530

    new files are added

--- LinkedLists/LLintro.java
+++ LinkedLists/LLintro.java
@@ -3,68 +3,87 @@ import java.util.*;
 public class LLintro {
 
     public static class Node {
-
         int data;
         Node next;
 
         public Node(int data) {
-
             this.data = data;
             this.next = null;
         }
-
     }
 
-    public static Node Head;
-    public static Node Tail;
+    public Node head;
+    public Node tail;
+    public static int size;
 
     public void addFirst(int data) {
-
         Node newNode = new Node(data);
-        if (Head == null) {
-
-            Head = Tail = newNode;
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
         }
-
-        newNode.next = Head;
-        Head = newNode;
+        size++;
     }
 
     public void addLast(int data) {
-
         Node newNode = new Node(data);
-        if (Head == null) {
-            Head = Tail = newNode;
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
         }
-
-        newNode.next = Tail;
-
-        Tail = newNode;
+        size++;
     }
 
-    public void Print() {
-
-        if (Head == null) {
+    public void print() {
+        if (head == null) {
             System.out.println("List is empty");
             return;
         }
 
-        Node temp = Head;
+        Node temp = head;
         while (temp != null) {
-            System.out.println(temp.data+"->");
+            System.out.print(temp.data + "->");
             temp = temp.next;
         }
-        System.out.println();
+        System.out.println("null");
+    }
 
+    public void Add(int idx, int data) {
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+        while (temp != null && i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+
+        if (temp == null) {
+            System.out.println("Index out of bounds");
+        } else {
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+        size++;
     }
 
     public static void main(String[] args) {
+        LLintro linkedList = new LLintro();
+        linkedList.addFirst(1);
+        linkedList.addLast(2);
+        linkedList.addLast(3);
+        linkedList.addLast(4);
+        linkedList.addLast(5);
 
-        LinkedList<Integer>ll = new LinkedList<>();
-        ll.addFirst(1);
-        ll.addFirst(2);
-        ll.addLast(3);
-        ll.addLast(4);
-
+        linkedList.print();
+        System.out.println(size);
     }
 }
