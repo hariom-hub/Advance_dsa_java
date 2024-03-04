401938e614cbcced7ce03ba33dfc193b8834b634
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-03-04 17:31:12 +0530

    changes saved

---
+++ LinkedLists/swapNode.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class swapNode {
+
+    public static void SwapNodes(ArrayList<Integer> list,int k){
+
+        
+
+    }
+
+
+    public static void main(String[] args){
+
+        ArrayList<Integer>lists = new ArrayList<>();
+        lists.add(1);
+        lists.add(2);
+        lists.add(3);
+        lists.add(4);
+        lists.add(4);
+        int k = 2;
+
+
+        
+
+
+    }
+}
