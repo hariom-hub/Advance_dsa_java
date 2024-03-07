7d604ab092b5caa847064086721cda8e20b2db54
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-03-07 22:50:16 +0530

    stack implemented

---
+++ Stacks/stack1.java
@@ -0,0 +1,58 @@
+package Stacks;
+
+import java.util.*;
+
+public class stack1 {
+
+    static class stack {
+
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        public static boolean isEmpty() {
+
+            return list.size() == 0;
+        }
+
+        // push
+        public static void push(int data) {
+
+            list.add(data);
+        }
+
+        // pop
+        public static int pop() {
+
+            if (isEmpty()) {
+                return -1;
+            }
+
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+
+        // peak
+        public static int peek() {
+
+            if (isEmpty()) {
+                return -1;
+            }
+            return list.get(list.size() - 1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Stack st = new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+
+        while (!st.isEmpty()) {
+            System.out.println(st.peek());
+            st.pop();
+        }
+
+    }
+}
