0f0c7821090b4a3233c69cd282d86d00803f913e
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-03-30 15:54:03 +0530

    file saved

---
+++ Queue/Deque.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class Deque {
+
+    public static void main(String[] args) {
+
+        Deque<Integer>deq = new LinkedList<>();
+
+        deq.
+    }
+}
--- Queue/queuebyLL.java
+++ Queue/queuebyLL.java
@@ -1,4 +1,4 @@
-package Queue;
+
 
 public class queuebyLL {
 
