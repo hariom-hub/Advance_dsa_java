ccb0c23062ca2ddf61ddcb0b68dc521d304d9fb6
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-04-25 11:50:06 +0530

    file added successfully

---
+++ BinaryTrees/binaryTree.java
@@ -0,0 +1,11 @@
+package Advance_dsa_java.BinaryTrees;
+
+import  java.util.*;
+
+public class binaryTree {
+
+    public static void main(String[] args){
+
+        System.out.println("hello world");
+    }
+}
