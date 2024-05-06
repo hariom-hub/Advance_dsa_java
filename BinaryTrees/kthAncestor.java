90d84c6c95a783be7e533062c4c089e74c9e14d7
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-05-06 14:37:14 +0530

    kth Ancestor file added

---
+++ BinaryTrees/kthAncestor.java
@@ -0,0 +1,61 @@
+package Advance_dsa_java.BinaryTrees;
+
+public class kthAncestor {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int kAncestor(Node root, int n, int k) {
+
+        if (root == null) {
+            return -1;
+        }
+
+        if (root.data == n) {
+            return 0;
+        }
+
+        int leftDist = kAncestor(root.left, n, k);
+        int rightDist = kAncestor(root.right, n, k);
+
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        }
+
+        int height = Math.max(leftDist, rightDist);
+
+        if (height + 1 == k) {
+            System.out.println(root.data + " ");
+        }
+        return height + 1;
+    }
+
+    public static void main(String[] args) {
+
+       Node root = new Node(1);
+
+       root.left = new Node(2);
+       root.right = new Node(3);
+
+       root.left.left = new Node(4);
+       root.left.right = new Node(5);
+
+       root.right.left = new Node(6);
+       root.right.right = new Node(7);
+
+        int n = 5;
+        int k = 2;
+       kAncestor(root,n,k);
+
+    }
+}
+
