5a791925daccb922d8cd3560cf5b1297a34c8f60
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-05-09 23:24:15 +0530

    printing nodes from root to leaf nodes i.e, the path is being added

--- BinarySearchTree/printinRange.java
+++ BinarySearchTree/printinRange.java
@@ -1,5 +1,8 @@
 package Advance_dsa_java.BinarySearchTree;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class printinRange {
 
     static class Node {
@@ -31,6 +34,47 @@ public class printinRange {
         }
     }
 
+    public static void Inorder(Node root) {
+
+        if (root == null) {
+
+            return;
+        }
+        Inorder(root.left);
+        System.out.print(root.data + " ");
+        Inorder(root.right);
+    }
+
+
+    public static void printPath(ArrayList<Integer> path) {
+
+
+        for(int i=0;i<path.size();i++){
+
+            System.out.print(path.get(i)+"->");
+        }
+        System.out.println("NULL");
+
+    }
+
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path) {
+
+        if (root == null) {
+            return;
+        }
+
+        path.add(root.data);
+
+        if (root.left == null && root.right == null) {
+
+            printPath(path);
+        }
+
+        printRoot2Leaf(root.left, path);
+        printRoot2Leaf(root.right, path);
+        path.remove(path.size() - 1);
+    }
+
     public static void main(String[] args) {
 
         Node root = new Node(8);
@@ -38,13 +82,12 @@ public class printinRange {
         root.right = new Node(10);
         root.left.left = new Node(3);
         root.left.right = new Node(6);
-        root.left.left.left = new Node(1);
-        root.left.left.right = new Node(4);
         root.right.right = new Node(11);
         root.right.right.right = new Node(14);
 
 
-        printInRange(root, 5, 12);
+//        printInRange(root, 5, 12);
+        printRoot2Leaf(root, new ArrayList<>());
 
 
     }
