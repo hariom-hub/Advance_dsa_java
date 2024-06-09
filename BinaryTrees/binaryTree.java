0e9b0302312e9e93f203ef1054f9020107dd1fc6
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-06-09 16:35:51 +0530

    pq directory added

--- BinaryTrees/binaryTree.java
+++ BinaryTrees/binaryTree.java
@@ -130,7 +130,7 @@ public class binaryTree {
 
         public static void main(String[] args) {
 
-            // int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6 };
+            // q
             // BinaryTree tree = new BinaryTree();
             // Node root = tree.buildTree(nodes);
             // System.out.println(root.data);
