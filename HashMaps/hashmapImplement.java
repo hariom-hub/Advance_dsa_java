339f50450ed262e7ea3ee13776ed500bd85d66d1
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-06-21 09:25:29 +0530

    files saved

---
+++ HashMaps/hashmapImplement.java
@@ -0,0 +1,36 @@
+package Advance_dsa_java.HashMaps;
+
+import java.util.*;
+
+public class hashmapImplement {
+
+    static class HashMap<K,V>{
+
+        private class Node{
+            K key;
+            V value;
+            private int size;  //n
+            private LinkedList<Node>buckets[];        //N
+
+            @SuppressWarnings("unchecked")
+
+            public void HashMap(){
+
+                public void put(K key,V value){
+
+                }
+                public boolean containsKey(int key){
+
+                    return false;
+                }
+
+            }
+
+        }
+    }
+
+    public static void main(String[] args){
+
+
+    }
+}
