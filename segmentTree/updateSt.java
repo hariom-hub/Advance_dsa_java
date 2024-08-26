16af442174575fb4336f7400496f67e0921bc90b
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-08-26 09:51:40 +0530

    files saved

--- segmentTree/updateSt.java
+++ segmentTree/updateSt.java
@@ -1,6 +1,6 @@
 package Advance_dsa_java.segmentTree;
 
-public class queryonBst {
+public class updateSt {
 
     static int[] tree;
 
@@ -9,20 +9,45 @@ public class queryonBst {
         tree = new int[4 * n];
     }
 
-    public static int buildst(int[] arr, int sti, int start, int end) {
+    public static int builtBst(int[] arr, int sti, int start, int end) {
 
         if (start == end) {
             tree[sti] = arr[start];
             return arr[start];
         }
+
         int mid = (start + end) / 2;
-        buildst(arr, 2 * sti + 1, start, mid);
-        buildst(arr, 2 * sti + 2, mid + 1, end);
+        builtBst(arr, 2 * sti + 1, start, mid);
+        builtBst(arr, 2 * sti + 2, mid + 1, end);
 
         tree[sti] = tree[2 * sti + 1] + tree[2 * sti + 2];
+
         return tree[sti];
     }
 
+    public static void updateUtil(int sti, int si, int sj, int idx, int diff) {
+
+        if (idx < si || idx > sj) {
+            return;
+        }
+        tree[sti] += diff;
+
+        if (si != sj) {
+            int mid = (si + sj) / 2;
+            //left
+            updateUtil(2 * sti + 1, si, mid, idx, diff);
+            //right
+            updateUtil(2 * sti + 2, mid + 1, sj, idx, diff);
+        }
+    }
+
+    public static void update(int[] arr, int idx, int newVal) {
+
+        int n = arr.length;
+        int diff = newVal - arr[idx];
+        updateUtil(0, 0, n - 1, idx, diff);
+
+    }
     public static int getSumUtil(int[] arr, int i, int si, int sj, int qi, int qj) {
 
         int n = arr.length;
@@ -54,10 +79,12 @@ public class queryonBst {
 
     public static void main(String[] args) {
 
-        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         int n = arr.length;
         init(n);
-        buildst(arr, 0, 0, n - 1);
-        System.out.println(getSum(arr, 2, 7));
+        builtBst(arr, 0, 0, n - 1);
+        System.out.println(getSum(arr,2,5));
+        update(arr,2,5);
+        System.out.println(getSum(arr,2,5));
     }
 }
