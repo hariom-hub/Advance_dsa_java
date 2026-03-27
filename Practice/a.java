228b8419505993b43924be838a180340919b499a
Hariom Singh Thakur <126336846+hariom-hub@users.noreply.github.com>
2026-03-27 12:30:15 +0000

    files saved

---
+++ Practice/a.java
@@ -0,0 +1,8 @@
+package Practice;
+
+public class a {
+    public static void main(String[] args) {
+        System.out.println("hello hariom");
+    }
+    
+}
---
+++ Practice/oop.java
@@ -0,0 +1,37 @@
+
+package Practice;
+import java.util.*;
+public class oop{
+
+
+    public static class Data{
+
+        String name;
+        int age;
+
+        public void GetData(){
+            System.out.println(this.name);
+            System.out.println(this.age);
+        }
+        // constructor
+        public Data(String name, int age){
+            this.age = age;
+            this.name = name;
+        }
+        // passing constructor as argument
+        // there is no destructor in java because of garbage collector
+        
+        public Data(Data d){
+            System.out.println(d.age);
+            System.out.println(d.name);
+        }
+
+    }
+    public static void main(String[] args){
+        System.out.println("hello hariom");
+        Data d1= new Data("harru",20);
+        Data d2 = new Data(d1);
+       
+        
+    }
+}
